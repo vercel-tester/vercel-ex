336f65a0a563fedcf90374defb8c05f1c8538354
fredrik <fcarlsson31@gmail.com>
2024-12-06 12:43:25 +0300

    ex comm

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Merhaba Dünya!</h1>
+      <p>Vercel üzerinde ilk deploy işlemim.</p>
+    </div>
+  );
+}
